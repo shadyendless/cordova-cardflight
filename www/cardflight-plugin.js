8755837420d25ad323a8f57470fa2669e98a9978
Jacob Foster <dalindeck@gmail.com>
2015-10-09 18:06:57 -0700

    Changed the exports.

--- www/cardflight-plugin.js
+++ www/cardflight-plugin.js
@@ -1,9 +1,11 @@
-function Cardflight() {}
+var Cardflight = {}
 
-Cardflight.prototype.init = function(APIToken, AccountToken, completionCallback) {
+Cardflight.init = function(APIToken, AccountToken, completionCallback) {
   var failureCallback = function(error) {
     console.log(error);
   };
 
   cordova.exec(completionCallback, failureCallback, "Cardflight", "init", APIToken, AccountToken);
 };
+
+module.exports = Cardflight;
